09da04e033c27daa64afd6c69060538f1f8a415b
jkljggggg <bbbdaru@gmail.com>
2025-07-02 20:57:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "7574196388:AAHZarLmsDRR1opNWiIfFQfAJvwE2Fp4zNM")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "FEGELD-ZBZEGV-OXJXNG-JDXEZX-ARQ")
+REPO_BOT = os.environ.get("REPO_BOT", "https://files.catbox.moe/k3q4k0.jpg")
+OWNER = os.environ.get("OWNER", "7241722965")
+BOT_NAME = os.environ.get("BOT_NAME", "Pvideovbot")
+UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "RAJAmusic67")
